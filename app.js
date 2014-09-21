5ed7011f3766077909e97df03bacb39bca3e035d
macikokoro <betwinsouls@gmail.com>
2014-09-21 13:19:05 -0700

    adding working signup with validation on submit

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-angular.module('myApp', [])
+var myApp = angular.module('myApp', [])
+
 .directive('ensureUnique', ['$http', function($http) {
   return {
     require: 'ngModel',
     link: function(scope, ele, attrs, c) {
-      scope.$watch(attrs.ngModel, function() {
+      scope.$watch(attrs.ngModel, function(n) {
+        if (!n) return;
         $http({
           method: 'POST',
           url: '/api/check/' + attrs.ensureUnique,
           data: {'field': attrs.ensureUnique}
-        }).success(function(data, status, headers, cfg) {
+        }).success(function(data) {
           c.$setValidity('unique', data.isUnique);
-        }).error(function(data, status, headers, cfg) {
+        }).error(function(data) {
           c.$setValidity('unique', false);
         });
       });
     }
   };
-}]);
+}])
+
+.controller('signupController', function($scope) {
+  $scope.submitted = false;
+  $scope.signupForm = function() {
+    if($scope.signup_form.$valid) {
+      //Submit as normal
+    } else {
+      $scope.signup_form.submitted = true;
+    }
+  };
+});
